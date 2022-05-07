d07a233ae69efd12d672dac6fd4243b1032c9b04
KeeYoo <poronko2045@gmail.com>
2022-05-07 16:00:27 +0300

    Project was created. Ruleset added.

---
+++ M2L1HW/M2L1HW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M2L1HW
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
